da7e295990aba9b41d12543a3322b78941c34916
alamincoders <developeralamin03@gmail.com>
2022-10-08 13:42:09 +0600

    login and register logic part-1 successfull

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -9,20 +10,33 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide a email"],
     unique: true,
-    match: [
-      /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))&/,
-      "Please provide a valid email address",
-    ],
   },
   password: {
     type: String,
     required: [true, "Please provide a password"],
     minLength: 6,
+    /*  match: [
+      /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))&/,
+      "Please provide a valid password",
+    ], */
     select: false,
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPasswords = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const connectDB = require("./config/db");
+
+// connect db
+connectDB();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +12,11 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 
-app.listen(port, () => {
-  console.log("server listening on port " + port);
+const server = app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
+
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`Logged Error : ${error}`);
+  server.close(() => process.exit(1));
 });
