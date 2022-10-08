108d35ab9ff7abaddb0c1af49e8c3371a79121af
alamincoders <developeralamin03@gmail.com>
2022-10-08 11:58:05 +0600

    controllers and routes setup

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
+
 app.listen(port, () => {
   console.log("server listening on port " + port);
 });
